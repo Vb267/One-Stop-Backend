80b75b02e61144240f0ee3e3252a9f4233aa2d86
rohanstomar11 <rohanstomar11@gmail.com>
2024-01-17 00:50:13 +0530

    search and pagination

--- oneStop/oneStop/settings.py
+++ oneStop/oneStop/settings.py
@@ -60,6 +60,11 @@ CORS_ALLOWED_ORIGINS = [
 
 ROOT_URLCONF = 'oneStop.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
